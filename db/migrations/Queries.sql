4842d7a53f593fa51fbb0a64ceb87fca33db138a
M.J <marita.jose.ch@gmail.com>
2019-04-02 23:37:17 +0200

    Agregando mas queries ---- SELECT AND INSERT--- ejercicio Manipulating Table Data

--- db/migrations/Queries.sql
+++ db/migrations/Queries.sql
@@ -1,2 +1,13 @@
+
+/* crear base de dato */
 CREATE DATABASE bookmark_manager;
+
+/* Creando una tabla */
 CREATE TABLE bookmarks(id SERIAL PRIMARY KEY, url VARCHAR(60));
+
+/*seleccionar todo los datos de las tablas*/
+SELECT * FROM bookmarks;
+
+/* Introducir valores a las tablas*/
+
+INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');
